bc515646e1863631ee9ad5510d1b7d04cc05aced
Christopher <chris.brower.96@gmail.com>
2023-02-01 11:33:41 -0700

    updated with defer

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+
+//document.querySelector('h1').setAttribute('id', 'victory');
+
+//document.querySelector('h1').innerHTML = '<h1 id="victory">';
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Chris is the champion";
+
+document.body.append(newHeader);
